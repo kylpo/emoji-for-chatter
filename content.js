5ba70b123ad23939e0175bde3fa8ddfd3a656d87
Kyle Poole <2kylepoole@gmail.com>
2014-08-21 08:20:52 -0700

    [added] emojify.js, and it successfully converts emoji text to images in feedItems before DOM is shown

--- content.js
+++ content.js
@@ -1,1 +1,13 @@
-alert('HAI!');
+console.log('HAI!');
+
+//debugger;
+emojify.setConfig({
+    img_dir: chrome.extension.getURL("bower_components/emojify.js/images/emoji")
+});
+
+var feedItems = document.getElementsByClassName('feeditemcontent');
+
+for (var i = 0; i < feedItems.length; i++) {
+    emojify.run(feedItems.item(i));
+}
+
