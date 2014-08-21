6ac8f4a2988982fc2124e9d5008f56040d60fdff
Kyle Poole <2kylepoole@gmail.com>
2014-08-21 09:02:52 -0700

    [added] jquery.at for publisher textarea. Still primitive, but functional enough to prove the idea.

--- content.js
+++ content.js
@@ -11,3 +11,13 @@ for (var i = 0; i < feedItems.length; i++) {
     emojify.run(feedItems.item(i));
 }
 
+$('#publishereditablearea').atwho({
+    at: ":",
+
+//    search_key: "nickname",
+//    tpl:"<li data-value=':${nickname}:'>${nickname} <img src=chrome.extension.getURL('bower_components/emojify.js/images/emoji/${nickname}.png')  height='20' width='20' /></li>",
+//    insert_tpl:"<img src='http://a248.e.akamai.net/assets.github.com/images/icons/emoji/${nickname}.png'  height='20' width='20' />",
+    show_the_at: false,
+    'start_with_space': false,
+    data:['smile', 'coffee', 'shower']
+});
