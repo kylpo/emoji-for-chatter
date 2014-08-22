3d9825b435d16b23c79ab71f987a16d9dbd246cd
Kyle Poole <2kylepoole@gmail.com>
2014-08-21 21:22:32 -0700

    [fixed] emojify all newly added posts and comments to feed

--- content.js
+++ content.js
@@ -5,13 +5,34 @@ emojify.setConfig({
     img_dir: chrome.extension.getURL("bower_components/emojify.js/images/emoji")
 });
 
+
+// emojify all feed items on page
 var feedItems = document.getElementsByClassName('feeditemcontent');
 
 for (var i = 0; i < feedItems.length; i++) {
     emojify.run(feedItems.item(i));
 }
 
-$('#publishereditablearea').atwho({
+// emojify inserted nodes if they are a feed item or feed item comment
+document.getElementById('feedwrapper').addEventListener('DOMNodeInserted', function(event) {
+    if (event.target.classList.contains("feeditem") || event.target.classList.contains("feeditemcomment")) {
+        emojify.run(event.target);
+    }
+});
+
+
+
+//document.getElementsByClassName('feedcontainer')[0].addEventListener('DOMNodeInserted', function(event) {
+//    if (event.target.classList.contains("feeditem") || event.target.classList.contains("feeditemcomment")) {
+//        emojify.run(event.target);
+//    }
+//});
+
+//$('.feeditemcontent').on('load', function(event) {
+//    console.log(event);
+//});
+
+var atwhoConfig = {
     at: ":",
 
 //    search_key: "nickname",
@@ -20,4 +41,35 @@ $('#publishereditablearea').atwho({
     show_the_at: false,
     'start_with_space': false,
     data:['smile', 'coffee', 'shower']
-});
+};
+
+//$('#publishereditablearea').atwho(atwhoConfig);
+
+//$('.cxnewcommenttext').atwho(atwhoConfig);
+
+//$('.feedcontainer').on('change', function(event) {
+//    console.log(event);
+//    var feedItems = document.getElementsByClassName('feeditemcontent');
+//
+//    for (var i = 0; i < feedItems.length; i++) {
+//        emojify.run(feedItems.item(i));
+//    }
+//});
+//debugger;
+
+//$('.feedcontainer').on('DOMNodeInserted', '.feeditem', function(event) {
+//    if (event.target.classList.contains("feeditem") || event.target.classList.contains("feeditemcomment")) {
+//        emojify.run(event.target);
+//    }
+//});
+
+//$('.feedcontainer')[0].addEventListener('DOMNodeInserted', function(event) { console.log(event); });
+
+
+//$('#feedwrapper').addEventListener('DOMNodeInserted', function(event) { console.log(event); });
+//    var feedItems = document.getElementsByClassName('feeditemcontent');
+//
+//    for (var i = 0; i < feedItems.length; i++) {
+//        emojify.run(feedItems.item(i));
+//    }
+//});
